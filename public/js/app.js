b4a19e2d82325b53eaa10cb3371b5e1e10e43eba
Daniel <danielhernandez16@hotmail.com>
2015-01-06 01:50:24 -0500

    Add GUI Ficha Tecnica Product

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,13 @@
       category: "Esferos Publicitarios",
       colors: ["Rojo", "Azul", "Verde"],
       caracteristic: [],
-      description: "Un esfero de punta suave que a su vez brinda la oportunidad de ver lo que se escribe en la oscuridad."
+      description: "Un esfero de punta suave que a su vez brinda la oportunidad de ver lo que se escribe en la oscuridad.",
+      height: "15cm",
+      width: "1cm",
+      material: "plastico",
+      typePrint: "Impresion en Tinta Plastica",
+      areaPrint: "60 mm x 7.5 mm",
+      flete: "Incluido por compras mayores a 250000"
     };
   });
 })();
