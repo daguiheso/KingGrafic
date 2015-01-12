77c2c4e3cdef87baf3ae47d6032bbc1efc8d26a9
Daniel <danielhernandez16@hotmail.com>
2015-01-12 11:15:29 -0500

    Add Images
    el2

--- public/js/app.js
+++ public/js/app.js
@@ -5,17 +5,19 @@
     this.esfero = {
       id: "001",
       name: "Esfero Linterna 2",
-      nameimg: "esferosp/el2-1",
+      nameimg: "el2-1",
       category: "Esferos Publicitarios",
       colors: ["Rojo", "Azul", "Verde"],
       caracteristic: [],
-      description: "Un esfero de punta suave que a su vez brinda la oportunidad de ver lo que se escribe en la oscuridad.",
+      description: ["Un esfero de punta suave que a su vez brinda la oportunidad de ver lo que se escribe en la oscuridad.", "Cuenta con linterna integrada."],      
       height: "15cm",
       width: "1cm",
       material: "plastico",
       typePrint: "Impresion en Tinta Plastica",
       areaPrint: "60 mm x 7.5 mm",
-      flete: "Incluido por compras mayores a 250000"
+      flete: "Incluido por compras mayores a 250000",
+      images: [ "el2-2", "el2-3", "el2-4" ]
     };
   });
 })();
+
