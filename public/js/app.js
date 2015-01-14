b640f9bf7490e726f5d5496568bbd928308a993a
Daniel <danielhernandez16@hotmail.com>
2015-01-14 18:31:40 -0500

    Create directive for comments witch controller
    directive with funcionality

--- public/js/app.js
+++ public/js/app.js
@@ -26,30 +26,13 @@
     this.selectTab = function(tab){
       this.tab = tab;
     };
-  });  
+  }); 
 
-  app.controller('CommentsController', function(){
-    this.comments = [];
-    this.comment = {};
-    this.show = false;
-
-    this.toggle = function () {
-      this.show = !this.show;
-    };
-
-    this.anonymousChanged = function(){
-      if (this.comment.anonymous) {
-        this.comment.email = "";
-      }
-    };
-
-    //tampoco recibe params de entrada porque vamos a trabajar sobre el controller
-    this.addComment = function(){
-      this.comment.date = Date.now();
-      this.comments.push(this.comment); //.push() ingresa un elemento en la ultima posicion del array
-      this.comment = {}; //limpiar o resetear el comentario despues de agregado
-    };
-
+  app.directive('categoriaesferoData', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'partials/categoriaesfero-data.html'
+    }
   });
 
   app.directive('descriptionData', function(){
@@ -78,6 +61,36 @@
       restrict: 'E',
       templateUrl: 'partials/imagen-data.html'
     }
+  });
+
+  app.directive('commentsData', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'partials/comments-data.html',
+      controller: function(){
+        this.comments = [];
+        this.comment = {};
+        this.show = false;
+
+        this.toggle = function () {
+          this.show = !this.show;
+        };
+
+        this.anonymousChanged = function(){
+          if (this.comment.anonymous) {
+            this.comment.email = "";
+          }
+        };
+
+        //tampoco recibe params de entrada porque vamos a trabajar sobre el controller
+        this.addComment = function(){
+          this.comment.date = Date.now();
+          this.comments.push(this.comment); //.push() ingresa un elemento en la ultima posicion del array
+          this.comment = {}; //limpiar o resetear el comentario despues de agregado
+        };
+      },
+      controllerAs: 'cmtsCtrl'
+    };
   });
 
   app.filter('imageify', function () {
