8552393836369fe76b8098779a41ec84cb3acb52
Daniel <danielhernandez16@hotmail.com>
2015-01-14 19:08:36 -0500

    Create a module for controllers

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,9 @@
 (function () {
-  var app = angular.module('penPublicity', []);
+  var app = angular.module('penPublicity', [
+    'kinggrafic.controllers'
+  ]);
 
-  app.controller('PenPController', function () {
-    this.esfero = {
-      id: "001",
-      name: "Esfero Linterna 2",
-      nameimg: "el2-1",
-      category: "Esferos Publicitarios",
-      colors: ["Rojo", "Azul", "Verde"],
-      caracteristic: [],
-      description: ["Un esfero de punta suave que a su vez brinda la oportunidad de ver lo que se escribe en la oscuridad.", "Cuenta con linterna integrada."],      
-      height: "15cm",
-      width: "1cm",
-      material: "plastico",
-      typePrint: "Impresion en Tinta Plastica",
-      areaPrint: "60 mm x 7.5 mm",
-      flete: "Incluido por compras mayores a 250000",
-      images: [ "el2-2", "el2-3", "el2-4" ]
-    };
-  });
-
-  app.controller('TabsController', function(){
-    this.tab = 1;
-
-    this.selectTab = function(tab){
-      this.tab = tab;
-    };
-  }); 
+  
 
   app.directive('categoriaesferoData', function(){
     return{
@@ -92,6 +69,7 @@
       controllerAs: 'cmtsCtrl'
     };
   });
+
 
   app.filter('imageify', function () {
     return function (input, ext) {
---
+++ public/js/controllers.js
@@ -0,0 +1,30 @@
+(function() {
+	// 2 param es el arreglo de las dependencias 
+	angular.module('kinggrafic.controllers', []) //sin  ;  al final para tener chainmethods
+		.controller('PenPController', function () {
+		  this.esfero = {
+		    id: "001",
+		    name: "Esfero Linterna 2",
+		    nameimg: "el2-1",
+		    category: "Esferos Publicitarios",
+		    colors: ["Rojo", "Azul", "Verde"],
+		    caracteristic: [],
+		    description: ["Un esfero de punta suave que a su vez brinda la oportunidad de ver lo que se escribe en la oscuridad.", "Cuenta con linterna integrada."],      
+		    height: "15cm",
+		    width: "1cm",
+		    material: "plastico",
+		    typePrint: "Impresion en Tinta Plastica",
+		    areaPrint: "60 mm x 7.5 mm",
+		    flete: "Incluido por compras mayores a 250000",
+		    images: [ "el2-2", "el2-3", "el2-4" ]
+		  };
+		})
+
+		.controller('TabsController', function(){
+		  this.tab = 1;
+
+		  this.selectTab = function(tab){
+		    this.tab = tab;
+		  };
+		}); 
+})();
