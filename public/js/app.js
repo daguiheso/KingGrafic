385ef1f2e333648418945fd73a2a13289ccdb20b
Daniel <danielhernandez16@hotmail.com>
2015-01-21 01:52:11 -0500

    Add ngRoute and basic route to load pen
    Injection dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,23 @@
 (function () {
 
   var app = angular.module('penPublicity', [
+  	'ngRoute', 
     'kinggrafic.controllers',
     'kinggrafic.directives',
     'kinggrafic.filters'
   ]);
+
+  //el modulo routeProvider esta siendo injectado a la funcion routeProvider
+  app.config(['$routeProvider', function($routeProvider){
+  	$routeProvider   //lamado de metodos encadenados
+  		.when('/', {  // when me hace match de la ruta  //el  /  => ruta principal
+  			templateUrl: 'views/esfero.html', //objeto de configuracion de la ruta 
+  			controller: 'PenPController', //injectando el controlador a esta ruta 
+  			controllerAs: 'penPCtrl'
+  		})
+  }]);
+
+
 
 })();
 
