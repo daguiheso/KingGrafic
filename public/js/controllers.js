cc645035fb05725a0953575b44ed89612f59fefa
Daniel <danielhernandez16@hotmail.com>
2015-01-26 15:12:47 -0500

    Add grid and fix CSS

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,12 +1,22 @@
-(function() {
+(function(_) {
 	// 2 param es el arreglo de las dependencias 
 	angular.module('kinggrafic.controllers', []) //sin  ;  al final para tener chainmethods
 		.controller('PenPsController', ['$scope', 'kinggraficService', function ($scope, kinggraficService){
 
 			kinggraficService.all().then(function (data){
 				$scope.penPs = data;
+				$scope.groupped = partition(data, 4);
 			});
+
+
+			function partition(data, n) {
+	        	return _.chain(data).groupBy(function (element, index) {
+	            	return Math.floor(index / n);
+	            }).toArray().value();
+	      	}
 		}])
+
+		
 
 		.controller('PenPController', ['$scope', '$routeParams', 'kinggraficService', function ($scope, $routeParams, kinggraficService) {
 			var name = $routeParams.name;
@@ -25,4 +35,4 @@
 		    this.tab = tab;
 		  };
 		}); 
-})();
+})(_);
